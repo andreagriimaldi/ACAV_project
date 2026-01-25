981bf5e1808cfe5da4f09decba03a626e04a11be
andreagrimaldi <ag@andreagrimaldi.eu>
2026-01-25 11:53:58 +0100

    Added physical acceleration/deceleration constraints

--- src/CPUVehicle.cpp
+++ src/CPUVehicle.cpp
@@ -16,12 +16,20 @@ double CPUVehicle::computeNewSpeed(double steer) const {
     double L = (map.getDim()/9) * 0.65;
 
     double a_lat_max = 0.05; //TUNING PARAMETER
+    double a_str_max = 0.3; //TUNING PARAMETER
 
     double v_max = maxspeed;
 
     if (std::abs(steer) > 0.001) {
         double v_lat = std::sqrt(a_lat_max * L / std::abs(std::tan(steer)));
         v_max = std::min(v_max, v_lat);
+    }
+
+    if (v_max > speed + a_str_max) {
+        v_max = speed + a_str_max;
+    }
+    else if (v_max < speed - a_str_max) {
+        v_max = speed - a_str_max;
     }
 
     return v_max;
